75009fe7547786cc0800748c5b4fc69f994ab292
Lailson <lailson.lima@gmail.com>
2020-04-19 02:16:22 -0300

    Automation Framework using selenium 4 practice

---
+++ src/test/java/com/newtours/tests/BookFlightTest.java
@@ -0,0 +1,60 @@
+package com.newtours.tests;
+
+import com.newtours.pages.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class BookFlightTest {
+
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setupDriver(){
+        System.setProperty("webdriver.chrome.driver", "/Users/lailson/chromedriver");
+        this.driver = new ChromeDriver();
+    }
+
+    @AfterTest
+    public void quitDriver(){
+        driver.quit();
+    }
+
+    @Test
+    public void registrationPageTest(){
+        RegistrationPage registrationPage = new RegistrationPage(driver);
+        registrationPage.goTo();
+        registrationPage.enterUserDetails("lailson", "pass");
+        registrationPage.enterUserCredentials("lailson", "pass");
+        registrationPage.submit();
+    }
+
+    @Test(dependsOnMethods = "registrationPageTest")
+    public void registrationConfirmationPageTest(){
+        RegistrationConfirmationPage registrationConfirmationPage = new RegistrationConfirmationPage(driver);
+        registrationConfirmationPage.goToFlightDetailsPage();
+    }
+
+    @Test(dependsOnMethods = "registrationConfirmationPageTest")
+    public void flightDetailsPageTest(){
+        FlightDetailsPage flightDetailsPage = new FlightDetailsPage(driver);
+        flightDetailsPage.selectPassengers("2");
+        flightDetailsPage.goToFindFlightsPage();
+    }
+
+    @Test(dependsOnMethods = "flightDetailsPageTest")
+    public void findFlightPageTest(){
+        FindFlightPage findFlightPage = new FindFlightPage(driver);
+        findFlightPage.submitFindFlightsPage();
+        findFlightPage.goToFlightConfirmationPage();
+    }
+
+    @Test(dependsOnMethods = "findFlightPageTest")
+    public void flightConfirmationPage(){
+        FlightConfirmationPage flightConfirmationPage = new FlightConfirmationPage(driver);
+        flightConfirmationPage.printConfirmation();
+    }
+
+}
